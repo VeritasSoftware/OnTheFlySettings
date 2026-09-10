65c4a8ee62c22f3501e95e9c54860de1cb970a2e
Shantanu Negi <shantanu_negi@hotmail.com>
2026-09-10 17:21:09 +1000

    added logging.

--- AspNetCore.OnTheFlySettings/OnTheFlySettings.cs
+++ AspNetCore.OnTheFlySettings/OnTheFlySettings.cs
@@ -8,11 +8,13 @@ namespace AspNetCore.OnTheFlySettings
         private TSettings _current;
         private TSettings? _old;
         private readonly object _lock = new();
+        private readonly ILogger<OnTheFlySettings<TSettings>>? _logger;
         public event Func<TSettings, TSettings, Task>? OnSettingsChanged;
 
-        public OnTheFlySettings(TSettings initial)
+        public OnTheFlySettings(TSettings initial, ILogger<OnTheFlySettings<TSettings>>? logger = null)
         {
             _current = initial;
+            _logger = logger;
         }
 
         public TSettings Current
@@ -32,6 +34,8 @@ namespace AspNetCore.OnTheFlySettings
 
         public void Replace(object newSettings)
         {
+            _logger?.LogInformation("Replacing settings with new settings.");
+
             if (newSettings is JsonElement jsonElement)
             {
                 var options = new JsonSerializerOptions
@@ -47,11 +51,14 @@ namespace AspNetCore.OnTheFlySettings
         public void Replace(TSettings newSettings)
         {
             lock (_lock)
-            {
+            {                
                 var oldSettings = _current;
+                _logger?.LogInformation("Replacing settings with new settings. Old settings: {OldSettings}, New settings: {NewSettings}", oldSettings, newSettings);
                 _old = oldSettings;
                 _current = newSettings;
+                _logger?.LogInformation("Settings replaced successfully. Old settings: {OldSettings}, New settings: {NewSettings}", oldSettings, newSettings);
                 OnSettingsChanged?.Invoke(oldSettings, newSettings);
+                _logger?.LogInformation("OnSettingsChanged event invoked successfully. Old settings: {OldSettings}, New settings: {NewSettings}", oldSettings, newSettings);
             }
         }
     }
